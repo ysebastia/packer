bc41c75c2f7e59d2bdd6dff0dfe7e3dbd5630f38
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2023-08-30 19:18:00 +0200

    fix(lint): missing source

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,10 +2,13 @@ terraform {
   required_version = ">= 0.12"
   required_providers {
     libvirt = {
-      source = "dmacvicar/libvirt"
+      source  = "dmacvicar/libvirt"
       version = "0.7.1"
     }
-    template = "~> 2.0"
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.0"
+    }
   }
 }
 
